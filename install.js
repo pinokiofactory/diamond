512b0b270517b984c0bb33f47a52e845c5d0f39c
6Morpheus6 <wanton-decay@gmx.net>
2025-12-03 00:38:41 +0100

    update
    - add bundle ai
    - call torch.js after requirements
    - remove fs.link
    - remove comments

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 module.exports = {
+  requires: {
+    bundle: "ai",
+  },
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
@@ -9,33 +11,25 @@ module.exports = {
         ]
       }
     },
-    // Delete this step if your project does not use torch
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        path: "app",
+        message: [
+          "uv pip install -r requirements.txt"
+        ]
+      }
+    },
     {
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
+          venv: "env",
+          path: "app",
+          // xformers: true
         }
-      }
-    },
-    // Edit this step with your custom install commands
-    {
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "pip install -r requirements.txt"
-        ]
-      }
-    },
-    {
-      method: "fs.link",
-      params: {
-        venv: "app/env"
       }
     }
   ]
