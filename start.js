149c8f24e768e165ab37caec864b353a98db9ca7
6Morpheus6 <wanton-decay@gmx.net>
2025-12-03 00:44:59 +0100

    update
    - add bundle ai
    - remove comments

--- start.js
+++ start.js
@@ -1,29 +1,25 @@
 module.exports = {
+  requires: {
+    bundle: "ai",
+  },
   daemon: true,
   run: [
     {
       method: "shell.run",
       params: {
         id: "shell",
-        venv: "env",                // Edit this to customize the venv folder path
+        venv: "env",
         env: {
           PYTORCH_ENABLE_MPS_FALLBACK: "1"
-        },                   // Edit this to customize environment variables (see documentation)
-        path: "app",                // Edit this to customize the path to start the shell from
+        },
+        path: "app",
         message: [
           "git checkout {{args.branch}}",
-          "{{args.branch === 'main' ? 'python src/play.py --pretrained' : null}}",    // Edit with your custom commands
-          "{{args.branch === 'csgo' ? 'python src/play.py' : null}}",    // Edit with your custom commands
+          "{{args.branch === 'main' ? 'python src/play.py --pretrained' : null}}",
+          "{{args.branch === 'csgo' ? 'python src/play.py' : null}}",
         ],
         on: [{
-          // The regular expression pattern to monitor.
-          // When this pattern occurs in the shell terminal, the shell will return,
-          // and the script will go onto the next step.
-          //"event": "/http:\/\/\\S+/",   
-          "event": "/(press enter|enter a number)/i",   
-
-          // "done": true will move to the next step while keeping the shell alive.
-          // "kill": true will move to the next step after killing the shell.
+          "event": "/(press enter|enter a number)/i",
           "done": true
         }]
       }
